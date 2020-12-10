726acc2249aa534a53a0d78bc03089e8875df0b9
SanneV <sanneversl@gmail.com>
2020-12-10 09:03:48 +0000

    nieuwe tables gemaakt

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,8 +1,52 @@
-DROP TABLE IF EXISTS products; 
-CREATE TABLE products (
+DROP TABLE IF EXISTS producten; 
+CREATE TABLE producten (
   ID SERIAL PRIMARY KEY,
   code VARCHAR(15),
-  name VARCHAR(255),
-  description TEXT,
-  price NUMERIC(10, 2)
+  naam VARCHAR(255),
+  becshrijving TEXT,
+  prijs NUMERIC(10, 2),
+  kleuren_id NUMERIC,
+  categorieen_id NUMERIC
 );
+
+DROP TABLE IF EXISTS kleuren; 
+CREATE TABLE kleuren (
+  ID SERIAL PRIMARY KEY,
+ kleur TEXT
+);
+
+DROP TABLE IF EXISTS categorieen; 
+CREATE TABLE categorieen (
+  ID SERIAL PRIMARY KEY,
+ categorie TEXT
+);
+
+DROP TABLE IF EXISTS ring; 
+CREATE TABLE ring (
+  ID SERIAL PRIMARY KEY,
+ categorie_id NUMERIC
+ maat TEXT
+);
+
+DROP TABLE IF EXISTS ketting; 
+CREATE TABLE ketting (
+  ID SERIAL PRIMARY KEY,
+ categorie_id NUMERIC
+ maat TEXT
+);
+
+DROP TABLE IF EXISTS oorbellen; 
+CREATE TABLE oorbellen (
+  ID SERIAL PRIMARY KEY,
+ categorie_id NUMERIC
+ maat TEXT
+);
+
+DROP TABLE IF EXISTS armband; 
+CREATE TABLE armband (
+  ID SERIAL PRIMARY KEY,
+ categorie_id NUMERIC
+ maat TEXT
+);
+
+JOIN kleuren_id ON kleuren.id = kleuren_id.producten;
