761716da3572ebe3f22b971f5e79e8d5051b2d5c
SanneV <sanneversl@gmail.com>
2020-12-16 14:55:01 +0000

    tables aangepast

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -3,50 +3,32 @@ CREATE TABLE producten (
   ID SERIAL PRIMARY KEY,
   code VARCHAR(15),
   naam VARCHAR(255),
-  becshrijving TEXT,
-  prijs NUMERIC(10, 2),
+  beschrijving TEXT,
+  prijs NUMERIC,
   kleuren_id NUMERIC,
-  categorieen_id NUMERIC
+  categorieen_id NUMERIC,
+  maat_id NUMERIC
 );
 
-DROP TABLE IF EXISTS kleuren; 
+DROP TABLE IF EXISTS kleuren;
 CREATE TABLE kleuren (
   ID SERIAL PRIMARY KEY,
  kleur TEXT
 );
 
-DROP TABLE IF EXISTS categorieen; 
+DROP TABLE IF EXISTS categorieen;
 CREATE TABLE categorieen (
-  ID SERIAL PRIMARY KEY,
- categorie TEXT
+    ID SERIAL PRIMARY KEY,
+    categorie TEXT
 );
 
-DROP TABLE IF EXISTS ring; 
-CREATE TABLE ring (
-  ID SERIAL PRIMARY KEY,
- categorie_id NUMERIC
- maat TEXT
+DROP TABLE IF EXISTS maten;
+CREATE TABLE maten (
+    ID SERIAL PRIMARY KEY,
+    maat TEXT
 );
 
-DROP TABLE IF EXISTS ketting; 
-CREATE TABLE ketting (
-  ID SERIAL PRIMARY KEY,
- categorie_id NUMERIC
- maat TEXT
-);
-
-DROP TABLE IF EXISTS oorbellen; 
-CREATE TABLE oorbellen (
-  ID SERIAL PRIMARY KEY,
- categorie_id NUMERIC
- maat TEXT
-);
-
-DROP TABLE IF EXISTS armband; 
-CREATE TABLE armband (
-  ID SERIAL PRIMARY KEY,
- categorie_id NUMERIC
- maat TEXT
-);
-
-JOIN kleuren_id ON kleuren.id = kleuren_id.producten;
+/*SELECT kleuren.kleur, producten.naam FROM producten
+JOIN kleuren ON kleuren.id = producten.kleuren_id;
+JOIN categorieen ON categorieen.id = producten.categorieen_id
+JOIN maten ON maten.id = producten.maat_id;*/
